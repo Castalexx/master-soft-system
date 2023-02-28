bf17b1ac3a3889d8992b4a09709b6bcb6be8f242
Claus Castillo <xclauscastillox@gmail.com>
2023-02-27 22:04:42 -0300

    Corregido

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,32 +3,38 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 
-const registerUser = async(req, res) => {
-    try{
-        const searchUser = await User.findOne({email: req.body.email})
-        if(searchUser){
-            res.json('El correo ya esta en uso')
-        } else {
-            const newUser = await User.create(req.body);
-            const userToken = jwt.sign({_id:newUser._id}, process.env.KEYJWT)
-            res.status(201).cookie({name:'userToken', val:userToken, httpOnly:true, expires: new Date(Date.now() + 90000)})
-            .json({successMessage: "Registered User", user:newUser})
-        }
-    }catch{
-        res.json('No se encontro')
-    }
-}
-
-
-
 module.exports = {
-    registerUser
-}
-
-
-/* const newUser = await User.create(req.body);
+    registerUser: async(req, res) => {
+      try{
+        const newUser = await User.create(req.body);
         console.log(newUser._id)
         const userToken = jwt.sign({_id:newUser._id}, process.env.KEYJWT)
         console.log(userToken)
         res.status(201).cookie('userToken', userToken, {httpOnly:true, expires: new Date(Date.now() + 90000)})
-        .json({successMessage: "Registered User", user:newUser}) */
+        .json({successMessage: "Registered User", user:newUser})
+      }catch(error){
+        res.status(404).json(error)
+      }
+    },
+  
+    loginUser: async (req, res)=>{
+      const user = await User.findOne({email:req.body.email})
+      if(!user){
+          res.status(400).json({error: "Email/Password incorrecto"})
+          console.log('no existe ese usuario')
+      }
+      try{
+          const passwordValida = await bcrypt.compare(req.body.password, user.password)
+          if(!passwordValida){
+              res.status(400).json({error: "Email/Password incorrecto"})
+          }else{
+            console.log(user)
+            const userToken = jwt.sign({_id:user._id}, SECRET)
+            console.log(userToken)
+            res.status(201).cookie('userToken', userToken )
+          }
+  
+      }catch(error){
+      }
+  }
+  }
